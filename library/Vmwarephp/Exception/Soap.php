9b64ff85c142910762a844b96da7578100d8f785
Robin Corps <robin@ngse.co.uk>
2014-10-10 13:12:42 +0100

    Handle Soap failure better

--- library/Vmwarephp/Exception/Soap.php
+++ library/Vmwarephp/Exception/Soap.php
@@ -24,9 +24,13 @@ class Soap extends \Exception {
 
 	private function makeFaultDetailsString($soapFault) {
 		$faults = array();
-		foreach ($soapFault->detail as $fault) {
-			$faults[] = "{$fault->enc_stype}: " . print_r($fault->enc_value, true);
-		}
+    if (property_exists($soapFault, 'detail'))
+    {
+      foreach ($soapFault->detail as $fault)
+      {
+        $faults[] = "{$fault->enc_stype}: " . print_r($fault->enc_value, true);
+      }
+    }
 		return count($faults) ? implode(', ', $faults) : '';
 	}
 }
