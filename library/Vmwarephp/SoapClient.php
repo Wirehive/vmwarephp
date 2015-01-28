a6ddcffdb215942605430a0a03b4b354aa696131
Robin Corps <robin@ngse.co.uk>
2015-01-28 12:58:53 +0000

    Fix method signature mismatch

--- library/Vmwarephp/SoapClient.php
+++ library/Vmwarephp/SoapClient.php
@@ -29,7 +29,7 @@ class SoapClient extends \SoapClient {
    *
    * @return mixed
    */
-  public function __soapCall($function_name, array $arguments, array $options = null, $input_headers = null, array &$output_headers = null)
+  public function __soapCall($function_name, $arguments, $options = null, $input_headers = null, &$output_headers = null)
   {
     $return = parent::__soapCall($function_name, $arguments, $options, $input_headers, $output_headers);
 
