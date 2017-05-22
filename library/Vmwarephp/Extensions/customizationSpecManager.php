97e4c93c1f7158fcec5a543b298976b5e1a81677
wladikz <wladikz@hotmail.com>
2017-05-22 15:52:41 +0300

    customizationSpecManager extension

---
+++ library/Vmwarephp/Extensions/customizationSpecManager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Vmwarephp\Extensions;
+
+class customizationSpecManager extends \Vmwarephp\ManagedObject {
+    function getInfo() {
+        $tmp= $this->info;
+        return $tmp;
+    }
+}
